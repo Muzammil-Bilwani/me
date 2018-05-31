fad8fc8ca19aed443d046a62c8311b29b23c8d94
Muzammil Bilwani <muzammil.asif@venturedive.com>
2018-05-31 15:56:31 +0500

    WIP

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
-  styleUrls: ['./skill.component.sass']
+  styleUrls: ['./skill.component.scss']
 })
 export class SkillComponent implements OnInit {
 
@@ -11,5 +11,12 @@ export class SkillComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  skills = `{
+    \nFrameworks_Libraries: ["Angular 2+/ JS", "ReactJs", "jQuery", "Ionic3", "Node.js", "Express.js", "SailsJs"],
+    \nFront_End: ["HTML5", "CSS3", "SASS", "LESS"],
+    \nLanguages: ["C", "Java", "JavaScript", "Typescript", "SQL"],
+    \nVersion_Controlling: ["Git"],
+  \n}`
 
 }
